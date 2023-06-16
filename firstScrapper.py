e93faf41260ddfe8516d61c8c850aa87c8c22dff
unknown <subhradeepray2017@gmail.com>
2023-06-16 23:17:34 +0530

    Scrapping Lipsum Site Using BeautifulSoup

--- firstScrapper.py
+++ firstScrapper.py
@@ -1,4 +1,16 @@
 import requests
-URL = "https://www.lipsum.com/"
-r = requests.get(URL)
-print(r.status_code)
+from bs4 import BeautifulSoup
+
+# Send a GET request to the website
+url = "https://www.lipsum.com"
+response = requests.get(url)
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the paragraphs containing the Lorem Ipsum text
+paragraphs = soup.find_all('p')
+
+# Extract and print the text content of the paragraphs
+for paragraph in paragraphs:
+    print(paragraph.get_text())
