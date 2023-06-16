1891d0ae6ae8b91706d9acb34930bce73bbc949d
unknown <subhradeepray2017@gmail.com>
2023-06-16 23:23:30 +0530

    Printing All Paragraphs and Div Tags Seperately

--- firstScrapper.py
+++ firstScrapper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Send a GET request to the website
-url = "https://www.lipsum.com"
+url = "https://www.example.com"
 response = requests.get(url)
 
 # Create a BeautifulSoup object
@@ -11,6 +11,20 @@ soup = BeautifulSoup(response.text, 'html.parser')
 # Find the paragraphs containing the Lorem Ipsum text
 paragraphs = soup.find_all('p')
 
+print('Printing all paragraphs')
+
 # Extract and print the text content of the paragraphs
 for paragraph in paragraphs:
     print(paragraph.get_text())
+
+# Find the div tags on the page
+div_tags = soup.find_all('div')
+
+print('\n\n')
+
+print('Printing all div tags')
+
+# Print the div tags
+for div in div_tags:
+    print(div)
+    print('-' * 50)  # Add a separator for readability
